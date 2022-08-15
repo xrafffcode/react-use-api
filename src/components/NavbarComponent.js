9999ed025ee12a8165a8a56fee2b867825e7e6ae
Muhamad Rafli <yusfiwawan2@gmail.com>
2022-08-15 09:30:55 +0700

    Add Form Tambah Data✨

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -6,7 +6,7 @@ const NavbarComponent = () => {
     return (
         <Navbar variant='dark' expand='lg'>
             <Container>
-                <Navbar.Brand href='#home'>CRUD Siswa</Navbar.Brand>
+                <Navbar.Brand as={Link} to='/'>CRUD Siswa</Navbar.Brand>
                 <Navbar.Toggle aria-controls='basic-navbar-nav' />
                 <Navbar.Collapse id='basic-navbar-nav'>
                     <Nav className='me-auto'>
--- src/pages/TambahData.js
+++ src/pages/TambahData.js
@@ -5,19 +5,49 @@ import axios from 'axios';
 
 
 export default function TambahData() {
+
+    const [data, setData] = React.useState({
+        nama: '',
+        kelas: '',
+        jurusan: ''
+    })
+
+    const handleChange = (event) => {
+        setData({
+            ...data,
+            [event.target.name]: event.target.value
+        })
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        axios.post(API_URL + 'api/siswa', data)
+            .then(res => {
+                console.log(res)
+                alert('Data berhasil ditambahkan');
+                window.location.href = '/';
+            }).catch(err => {
+                console.log(err)
+            }
+            )
+    }
+
     return (
         <Container className='mt-3'>
             <Form>
                 <Form.Group className="mb-3" >
                     <Form.Label>Nama</Form.Label>
-                    <Form.Control type="text" placeholder="Masukan Nama" />
+                    <Form.Control type="text" placeholder="Masukan Nama" name="nama" onChange={handleChange} />
                 </Form.Group>
 
                 <Form.Group className="mb-3">
                     <Form.Label>Kelas</Form.Label>
-                    <Form.Control type="text" placeholder="Masukan Kelas" />
+                    <Form.Control type="text" placeholder="Masukan Kelas" name="kelas" onChange={handleChange} />
                 </Form.Group>
-                <Button variant="primary" type="submit">
+                <Form.Group className="mb-3">
+                    <Form.Label>Jurusan</Form.Label>
+                    <Form.Control type="text" placeholder="Masukan Jurusan" name="jurusan" onChange={handleChange} />
+                </Form.Group>
+                <Button variant="primary" type="submit" onClick={handleSubmit}>
                     Submit
                 </Button>
             </Form>
